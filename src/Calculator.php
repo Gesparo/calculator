61f41c1ea01180d290e463597d5b3233c9bb09ed
Maxim Melnik <gesparo1@gmail.com>
2023-08-15 20:05:20 +0300

    Added calculator

---
+++ src/Calculator.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gesparo\Calculator;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+}
